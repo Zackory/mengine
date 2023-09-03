84be72416a5d289e5f66ac420c3120209599c1f9
Zackory Erickson <zackory@Zackorys-MacBook-Air.local>
2023-09-03 02:37:03 -0400

    four bar linkage

--- examples/lecture06_fourbarlinkage.py
+++ examples/lecture06_fourbarlinkage.py
@@ -9,13 +9,48 @@ env.set_gui_camera(look_at_pos=[0, 0, 0], yaw=30)
 
 fbl = m.URDF(filename=os.path.join(m.directory, 'fourbarlinkage.urdf'), static=True, position=[0, 0, 0.3], orientation=[0, 0, 0, 1])
 fbl.controllable_joints = [0, 1, 2]
-
+# Create a constraint for the 4th joint to create a closed loop
 fbl.create_constraint(parent_link=1, child=fbl, child_link=4, joint_type=m.p.JOINT_POINT2POINT, joint_axis=[0, 0, 0], parent_pos=[0, 0, 0], child_pos=[0, 0, 0])
+m.step_simulation(steps=20, realtime=False)
 
-# m.visualize_coordinate_frame()
+coupler_links = [1, 3, 5]
+
+# links = [1, 3]
+# global_points = []
+# previous_global_points = []
+# lines = []
+# 
+# for link in links:
+#     global_points.append(fbl.get_link_pos_orient(link)[0])
+#     previous_global_points.append(global_points[-1])
+#     point = m.Shape(m.Sphere(radius=0.02), static=True, position=global_points[-1], rgba=[0, 0, 1, 1])
 
 for i in range(10000):
-    j = np.radians(i)
-    fbl.control([j]*3)
+    fbl.control([np.radians(i)]*3)
+
+    if i % 5 == 0:
+        # Draw coupler curve
+        for j, link in enumerate(coupler_links):
+            p = fbl.get_link_pos_orient(link)[0]
+            color = [0, 0, 0, 1]
+            color[j] = 1
+            m.Shape(m.Sphere(radius=0.005), static=True, position=p, collision=False, rgba=color)
+
+#     if i > 3:
+#         for j, (link, global_position, previous_global_position) in enumerate(zip(links, global_points, previous_global_points)):
+#             p_new = fbl.get_link_pos_orient(link)[0]
+#             ic_vector_of_motion = p_new - previous_global_position
+#             ic_bisector = np.cross(ic_vector_of_motion, [0,1,0])
+#             ic_bisector = ic_bisector / np.linalg.norm(ic_bisector)
+#             previous_global_points[j] = p_new
+# 
+#             if j >= len(lines):
+#                 lines.append([])
+#                 lines[j].append(m.Line(p_new, p_new+ic_vector_of_motion*10, radius=0.005, rgba=[1, 0, 0, 0.5]))
+#                 lines[j].append(m.Line(p_new, p_new-ic_bisector/2, radius=0.005, rgba=[0, 0, 1, 0.5]))
+#             else:
+#                 lines[j][0] = m.Line(p_new, p_new+ic_vector_of_motion*10, radius=0.005, rgba=[1, 0, 0, 0.5], replace_line=lines[j][0])
+#                 lines[j][1] = m.Line(p_new, p_new-ic_bisector/2, radius=0.005, rgba=[0, 0, 1, 0.5], replace_line=lines[j][1])
+
     m.step_simulation(realtime=True)
 
--- examples/lecture06_ic.py
+++ examples/lecture06_ic.py
@@ -22,7 +22,7 @@ for position in local_points:
 
 rotation_center = [0.2, -0.2, 0]
 # rotation_center = [0, 0, 0]
-point_rc = m.Shape(m.Sphere(radius=0.02), static=True, position=rotation_center, rgba=[1, 0, 0, 1])
+point_rc = m.Shape(m.Sphere(radius=0.02), static=True, position=rotation_center + np.array([0,0,0.1]), collision=False, rgba=[1, 0, 0, 1])
 
 m.step_simulation(steps=30, realtime=True)
 
@@ -31,25 +31,25 @@ for i in range(100):
     p2, q2 = m.multiply_transforms(rotation_center, [0, 0, np.radians(i+1)], p-rotation_center, q)
     cube.set_base_pos_orient(p2, q2)
 
-    for i, (position, global_position, previous_global_position) in enumerate(zip(local_points, global_points, previous_global_points)):
+    for j, (position, global_position, previous_global_position) in enumerate(zip(local_points, global_points, previous_global_points)):
         p_new = cube.local_to_global_coordinate_frame(position)[0]
         ic_vector_of_motion = p_new - previous_global_position
         ic_bisector = np.cross(ic_vector_of_motion, [0,0,1])
         ic_bisector = ic_bisector / np.linalg.norm(ic_bisector)
-        previous_global_points[i] = p_new
+        previous_global_points[j] = p_new
 
-        if i >= len(lines):
+        if j >= len(lines):
             lines.append([])
-            # lines[i].append(m.Line(global_position, p_new, radius=0.005, rgba=[1, 1, 1, 0.5]))
-            # lines[i].append(m.Line(midpoint, midpoint-bisector/2, radius=0.005, rgba=[1, 0, 0, 0.5]))
-            lines[i].append(m.Line(p_new, p_new+ic_vector_of_motion*10, radius=0.005, rgba=[1, 0, 0, 0.5]))
-            lines[i].append(m.Line(p_new, p_new-ic_bisector/2, radius=0.005, rgba=[0, 0, 1, 0.5]))
+            # lines[j].append(m.Line(global_position, p_new, radius=0.005, rgba=[1, 1, 1, 0.5]))
+            # lines[j].append(m.Line(midpoint, midpoint-bisector/2, radius=0.005, rgba=[1, 0, 0, 0.5]))
+            lines[j].append(m.Line(p_new, p_new+ic_vector_of_motion*10, radius=0.005, rgba=[1, 0, 0, 0.5]))
+            lines[j].append(m.Line(p_new, p_new-ic_bisector/2, radius=0.005, rgba=[0, 0, 1, 0.5]))
         else:
-            # m.clear_visual_item(lines[i][0])
-            # lines[i][0] = m.Line(global_position, p_new, radius=0.005, rgba=[1, 1, 1, 0.5])
-            # lines[i][1] = m.Line(midpoint, midpoint-bisector/2, radius=0.005, rgba=[1, 0, 0, 0.5], replace_line=lines[i][1])
-            lines[i][0] = m.Line(p_new, p_new+ic_vector_of_motion*10, radius=0.005, rgba=[1, 0, 0, 0.5], replace_line=lines[i][0])
-            lines[i][1] = m.Line(p_new, p_new-ic_bisector/2, radius=0.005, rgba=[0, 0, 1, 0.5], replace_line=lines[i][1])
+            # m.clear_visual_item(lines[j][0])
+            # lines[j][0] = m.Line(global_position, p_new, radius=0.005, rgba=[1, 1, 1, 0.5])
+            # lines[j][1] = m.Line(midpoint, midpoint-bisector/2, radius=0.005, rgba=[1, 0, 0, 0.5], replace_line=lines[j][1])
+            lines[j][0] = m.Line(p_new, p_new+ic_vector_of_motion*10, radius=0.005, rgba=[1, 0, 0, 0.5], replace_line=lines[j][0])
+            lines[j][1] = m.Line(p_new, p_new-ic_bisector/2, radius=0.005, rgba=[0, 0, 1, 0.5], replace_line=lines[j][1])
 
     m.step_simulation(realtime=True)
 
