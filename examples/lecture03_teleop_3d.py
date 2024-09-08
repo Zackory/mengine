5c0cb461b66c6830a43eb71c908b6d6c788df368
Zackory Erickson <zackory@Zackorys-MacBook-Air-M3.local>
2024-09-08 16:33:24 -0400

    renaming lectures

--- examples/lecture03_teleop_3d.py
+++ examples/lecture03_teleop_3d.py
@@ -60,7 +60,7 @@ while True:
             for j, (p2, q2) in enumerate(coordinate_frames[i+1:]):
                 print('Euclidean between (%d) and (%d):' % (i, i+1+j), np.linalg.norm(p2 - p1))
                 print('Manhattan between (%d) and (%d):' % (i, i+1+j), np.sum(np.abs(p2 - p1)))
-                print('Chebyshev between (%d) and (%d):' % (i, i+1+j), np.max(p2 - p1))
+                print('Chebyshev between (%d) and (%d):' % (i, i+1+j), np.max(np.abs(p2 - p1)))
                 print('Quaternion distance between (%d) and (%d)' % (i, i+1+j), np.arccos(2*np.square(q1.dot(q2)) - 1))
         print('-'*20)
     if 'c' in keys:
